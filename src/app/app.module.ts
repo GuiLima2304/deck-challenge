b1d64f16422a4377f5594b9fe86febc9323a090f
Guilherme <gui.lima2304@gmail.com>
2023-04-11 19:12:04 -0300

    set up localstorage

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { FeedComponent } from './feed/feed.component';
 import { TweetsComponent } from './feed/components/tweets/tweets.component';
 import { NewTweetComponent } from './feed/components/new-tweet/new-tweet.component';
 import { FormsModule } from '@angular/forms';
+import { NavbarComponent } from './feed/components/navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     FeedComponent,
     TweetsComponent,
-    NewTweetComponent
+    NewTweetComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
--- src/app/feed/components/tweets/tweets.component.ts
+++ src/app/feed/components/tweets/tweets.component.ts
@@ -7,7 +7,10 @@ import { ChangeDetectorRef, Component, Input, SimpleChanges } from '@angular/cor
 })
 export class TweetsComponent {
   @Input() tweetSaved: string = '';
-  public listTweets: Array<string> = [];
+  // public listTweets: Array<string> = [];
+  public listTweets: Array<any> = [];
+  tweetSelecionado: any;
+
 
   constructor(private ref: ChangeDetectorRef) {
 
@@ -23,12 +26,32 @@ export class TweetsComponent {
           this.listTweets.push(element);
         });
       }
-      this.listTweets.push(this.tweetSaved);
-      localStorage.setItem("listTweets", JSON.stringify(this.listTweets))
+
+      this.listTweets.push({id: this.generateGuid(), tweet: this.tweetSaved});
+      localStorage.setItem("listTweets", JSON.stringify(this.listTweets));
       this.tweetSaved = '';
     }
   }
 
+  public selecioneItem(item: any) {
+    this.tweetSelecionado = item;
+  }
+
+  public deleteTweet() {
+    let list: [] = this.getList();
+    let finalList = list.filter((item: any) => item.id !== this.tweetSelecionado.id);
+
+    localStorage.setItem("listTweets", JSON.stringify(finalList));
+    window.location.reload();
+  }
+
+  private generateGuid() : string {
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+      var r = Math.random() * 16 | 0,
+        v = c == 'x' ? r : (r & 0x3 | 0x8);
+      return v.toString(16);
+    });
+  }
 
   public getList(): any {
       return JSON.parse(localStorage.getItem("listTweets") || '[]')
